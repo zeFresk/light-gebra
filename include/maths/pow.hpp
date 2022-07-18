b090efc0bd4e1d9e3617f87aeff03e3208d18c2f
zeFresk <8807862+zeFresk@users.noreply.github.com>
2022-07-18 18:18:30 +0200

    Fix int power problem

--- include/maths/pow.hpp
+++ include/maths/pow.hpp
@@ -7,9 +7,9 @@
 template <typename T, typename = std::enable_if_t<std::is_convertible_v<float, std::remove_reference_t<std::remove_cv_t<T>>>>>
 constexpr std::remove_reference_t<std::remove_cv_t<T>> pow(T&& v, int n) {
 	if (n == 0)
-		return std::remove_reference_t<std::remove_cv_t<T>>{1.f};
+		return std::remove_reference_t<std::remove_cv_t<T>>{static_cast<std::remove_reference_t<std::remove_cv_t<T>>>(1.f)};
 	if (n < 0)
-		return std::remove_reference_t<std::remove_cv_t<T>>{1.f} / pow(v, -n);
+		return std::remove_reference_t<std::remove_cv_t<T>>{static_cast<std::remove_reference_t<std::remove_cv_t<T>>>(1.f)} / pow(v, -n);
 	return (n % 2 == 0) ? pow(v * v, n / 2) : v * pow(v * v, (n-1)/2);
 }
 
